c45e0083a66af27e523ac717d6846e6cf680a67e
lierniortiz <65980093+lierniortiz@users.noreply.github.com>
2021-09-20 21:36:26 +0200

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    develop: {
+      port: 8545,
+    },
+    db: {
+      enabled: false,
+    },
+  },
+};
