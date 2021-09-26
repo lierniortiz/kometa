b2ad139410e870880c021440fdf257e6ed492ce2
lierniortiz <65980093+lierniortiz@users.noreply.github.com>
2021-09-26 21:07:19 +0200

    Update subir.js

--- src/js/subir.js
+++ src/js/subir.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", async function () {
   window.conexionWeb3();
   const ac = await getCuenta();
-  document.getElementById("cuenta").innerHTML = "Cuenta: " + ac;
+  if (ac != undefined)
+    document.getElementById("cuenta").innerHTML = "Cuenta: " + ac;
 });
