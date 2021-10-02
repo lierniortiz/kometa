6f79742466451d85678e7c66ba243246b1180228
lierniortiz <65980093+lierniortiz@users.noreply.github.com>
2021-10-02 19:22:56 +0200

    Add files via upload

---
+++ src/js/subir.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", async function () {
+  window.conexionWeb3();
+  const ac = await getCuenta();
+  if (ac != undefined)
+  	document.getElementById("cuenta").innerHTML = "Cuenta: " + ac;
+});
