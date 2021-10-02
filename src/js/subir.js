98a8abce4a2b878bdc2c9ffaf432e8b6315b2490
lierniortiz <65980093+lierniortiz@users.noreply.github.com>
2021-10-02 20:52:50 +0200

    Delete src directory

--- src/js/subir.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", async function () {
-  window.conexionWeb3();
-  const ac = await getCuenta();
-  if (ac != undefined)
-  	document.getElementById("cuenta").innerHTML = "Cuenta: " + ac;
-});
